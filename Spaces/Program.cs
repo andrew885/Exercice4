d3c828ae62ffe8faed95860bdfb2147f4c64f998
Andrew <Andrew@HOME-PC>
2021-10-19 08:49:42 +0300

    Add project files.

---
+++ Spaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Spaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (0 <= number && number <= 30)
+                Console.WriteLine("1ый промежуток");
+            else if (31 <= number && number <= 60)
+                Console.WriteLine("2ой промежуток");
+            else if (61 <= number && number <= 100)
+                Console.WriteLine("3ий промежуток");
+            else
+                Console.WriteLine("Я такого числа не знаю.");
+        }
+    }
+}
---
+++ While/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 200;
+            do
+            {
+                if (i % 17 == 0)
+                    Console.Write(i+", ");
+                i++;
+            }
+            while (i <= 500);
+        }
+    }
+}
